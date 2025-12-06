380306a72e97c97e4fb56a752c0709ad154449c8
John Syrinek <john.syrinek@gmail.com>
2025-12-06 15:29:58 -0600

    Layer ordering fix

--- src/render/renderViews.js
+++ src/render/renderViews.js
@@ -6,14 +6,14 @@ export function renderViews(svgRefs, plantStates, pixelsPerInch, options = {}) {
   const { topSvg, southSvg, westSvg } = svgRefs;
   const { showLabels = false, hiddenLayerCount = 0 } = options;
   const filtered = filterPlantStatesByHiddenLayers(plantStates, hiddenLayerCount);
-  const ordered = orderPlantStatesForStacking(filtered);
-  renderTopView(topSvg, ordered, pixelsPerInch, { showLabels });
-  renderSouthElevation(southSvg, ordered, pixelsPerInch, { showLabels });
-  renderWestElevation(westSvg, ordered, pixelsPerInch, { showLabels });
+  const topOrdered = orderTopViewPlantStates(filtered);
+  renderTopView(topSvg, topOrdered, pixelsPerInch, { showLabels });
+  renderSouthElevation(southSvg, filtered, pixelsPerInch, { showLabels });
+  renderWestElevation(westSvg, filtered, pixelsPerInch, { showLabels });
 }
 
-function orderPlantStatesForStacking(plantStates) {
-  // Draw shorter plants first so taller ones naturally layer on top.
+function orderTopViewPlantStates(plantStates) {
+  // Plan view layers purely by height: shorter first, taller last.
   return [...plantStates].sort((a, b) => {
     const heightDiff = (a.plant.height ?? 0) - (b.plant.height ?? 0);
     if (heightDiff !== 0) return heightDiff;
